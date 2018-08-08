2a85f1461ba9e1092eca0c35ea0f55def8885b15
nadjayanna <nadjayannabr@gmail.com>
2018-08-08 10:10:02 +0200

    add functionality to flip the cards

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 /*** Function that will allocate randonly the icons into the cards***/
-function allocateImages (icons){
-
-	//list of elementes where the icon will be allocate
-	const lis = $(".cards").find("i");
-
-	lis.each(function() {
+function allocateImages (icons, cards){
+	cards.each(function() {
 		//randonly select a icon from the vector of icons
 		const index = getRandomInt(icons.length);
   	$(this).toggleClass(icons[index]);
@@ -19,7 +15,22 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+//list of elementes where the icon will be allocate
+const cards = $(".cards").find("i");
+
 //vector with the cards icons
 let icons = ["fa-ambulance", "fa-bus-alt", "fa-wheelchair", "fa-frog", "fa-chess-knight", "fa-laptop-code", "fa-smile-wink", "fa-coffee", "fa-ambulance", "fa-bus-alt", "fa-wheelchair", "fa-frog", "fa-chess-knight", "fa-laptop-code", "fa-smile-wink", "fa-coffee"];
 
-allocateImages(icons);
+allocateImages(icons, cards);
+
+//add click listener to the cards
+$('.cards').on( 'click', 'li', function(e) {
+	//select the elemente where the icon will be display
+	var icon = e.currentTarget.childNodes[0];
+	//make the icon show
+	$(icon).toggleClass('icon-clicked');
+	$(icon).toggleClass('icon');
+	//change the card aspect
+	$(this).toggleClass('card');
+	$(this).toggleClass('card-clicked');
+});
